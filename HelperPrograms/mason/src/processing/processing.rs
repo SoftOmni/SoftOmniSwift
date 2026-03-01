29c9285929049862f077c9d535e369f3a759291e
Jasch-M <jaschdevelopment@gmail.com>
2026-03-01 22:41:48 +0100

    refactor(mason/processing):
    added verbose logging throughout

--- HelperPrograms/mason/src/processing/processing.rs
+++ HelperPrograms/mason/src/processing/processing.rs
@@ -4,6 +4,10 @@ use crate::processing::verbose_logging::{VERBOSE_LOGGER, VerboseLogger, verbose_
 use std::path::Path;
 
 pub fn process(arguments: &arguments::Arguments) -> usize {
+    VERBOSE_LOGGER.set_verbose(arguments);
+
+    verbose_log("BEGINNING PROCESSING");
+    verbose_log("RETRIEVING PATH FOR PROCESSING FROM ARGUMENTS DATA");
     if arguments.path().is_none() {
         eprintln!(
             "Path argument is required for processing.\n\
@@ -12,10 +16,19 @@ pub fn process(arguments: &arguments::Arguments) -> usize {
         return 0;
     }
 
+    verbose_log(&format!(
+        "RETRIEVED PATH '{}' FOR PROCESSING FROM ARGUMENTS DATA",
+        arguments.path().as_deref().unwrap()
+    ));
+
+    verbose_log("SELECTING PROCESSOR");
     if arguments.is_file_target() {
+        verbose_log("SELECTED FILE PROCESSOR");
         return process_file(arguments);
     }
 
+    verbose_log("SELECTED DIRECTORY PROCESSOR");
+    verbose_log("RETRIEVING ROOT DIRECTORY PATH");
     let path_str: &str = arguments.path().as_deref().unwrap();
     process_root_directory(arguments, Path::new(path_str))
 }
@@ -62,9 +75,11 @@ fn process_file(arguments: &arguments::Arguments) -> usize {
     let path_buffer = path.to_path_buf();
     let potential_parent_buffer = fs::canonicalize(path_buffer.clone());
 
-    if potential_parent_buffer.is_err() {
-        let potential_name = path_buffer.to_str();
     let result = process_root_directory(arguments, Path::new(path_str));
+    verbose_log(&format!(
+        "PROCESSING COMPLETELY FINISHED (FROM PROCESSOR). NUMBER OF FILES PROCESSED IS {}",
+        result
+    ));
 
     result
 }
@@ -80,9 +95,13 @@ pub(super) fn is_ok_name_according_to_filter(
     file_name: &str,
     directory_path: &Path,
 ) -> bool {
+    verbose_log(&format!("CHECKING IF FILE '{}' MATCHES FILTER (DEFAULT GLOB, CUSTOM GLOB OR PATTERN)", file_name));
+    verbose_log("TO DECIDE WHETHER TO PROCESS THE FILE OR NOT AND INCLUDE IT");
     let result = arguments.file_check()(arguments, file_name);
 
     if !result {
+        verbose_log(&format!("FILE '{}' DOES NOT MATCH FILTER", file_name));
+        verbose_log("SKIPPING FILE");
         let full_path_buffer = directory_path.join(file_name);
         let full_path = full_path_buffer.to_str();
         if full_path.is_none() {
@@ -94,138 +113,7 @@ pub(super) fn is_ok_name_according_to_filter(
         eprintln!("Warning, the file '{}' was skipped due to not matching the glob or regex pattern", full_path);
     }
 
+    verbose_log(&format!("FILE '{}' MATCHES FILTER", file_name));
+    verbose_log("PROCESSING FILE AND FILE WILL BE INCLUDED");
     result
-}
-
-fn sort_entries(entries: &mut Vec<DirEntry>, sorting_order: &SortingOrder) {
-    entries.sort_by(|lhs_entry, rhs_entry| match sorting_order {
-        SortingOrder::Alphabetical => lhs_entry.file_name().cmp(&rhs_entry.file_name()),
-        SortingOrder::ReverseAlphabetical => {
-            lhs_entry.file_name().cmp(&rhs_entry.file_name()).reverse()
-        }
-        SortingOrder::DateCreatedMostRecent => metadata_time_based_sort(
-            lhs_entry,
-            rhs_entry,
-            "creation time",
-            fs::Metadata::created,
-            |lhs_time, rhs_time| lhs_time.cmp(rhs_time),
-        ),
-        SortingOrder::DateCreatedLeastRecent => metadata_time_based_sort(
-            lhs_entry,
-            rhs_entry,
-            "creation time",
-            fs::Metadata::created,
-            |lhs_time, rhs_time| lhs_time.cmp(rhs_time).reverse(),
-        ),
-        SortingOrder::DateModifiedMostRecent => metadata_time_based_sort(
-            lhs_entry,
-            rhs_entry,
-            "modification time",
-            fs::Metadata::modified,
-            |lhs_time, rhs_time| lhs_time.cmp(rhs_time),
-        ),
-        SortingOrder::DateModifiedLeastRecent => metadata_time_based_sort(
-            lhs_entry,
-            rhs_entry,
-            "modification time",
-            fs::Metadata::modified,
-            |lhs_time, rhs_time| lhs_time.cmp(rhs_time).reverse(),
-        ),
-    });
-
-    fn metadata_time_based_sort<TimeAcquirer, Sorter>(
-        lhs_entry: &DirEntry,
-        rhs_entry: &DirEntry,
-        time_field_name: &str,
-        time_acquirer: TimeAcquirer,
-        sorter: Sorter,
-    ) -> Ordering
-    where
-        TimeAcquirer: Fn(&fs::Metadata) -> std::io::Result<std::time::SystemTime>,
-        Sorter: Fn(&std::time::SystemTime, &std::time::SystemTime) -> Ordering,
-    {
-        let lhs_metadata_result = lhs_entry.metadata();
-        let rhs_metadata_result = rhs_entry.metadata();
-
-        match (lhs_metadata_result, rhs_metadata_result) {
-            (Ok(lhs_metadata), Ok(rhs_metadata)) => {
-                let lhs_created_result = time_acquirer(&lhs_metadata);
-                let rhs_created_result = time_acquirer(&rhs_metadata);
-
-                match (lhs_created_result, rhs_created_result) {
-                    (Ok(lhs_time), Ok(rhs_time)) => sorter(&lhs_time, &rhs_time),
-                    (Ok(_), Err(rhs_time_error)) => {
-                        eprintln!(
-                            "Error: Fetching the {} for the file {} failed, therefore {} \
-                            will be considered last in ordering compared to entries without issues\n\
-                            The error that happened was {}",
-                            &time_field_name,
-                            &rhs_entry.path().display(),
-                            &rhs_entry.path().display(),
-                            rhs_time_error
-                        );
-                        Ordering::Greater
-                    }
-                    (Err(lhs_time_error), Ok(_)) => {
-                        eprintln!(
-                            "Error: Fetching the {} for the file {} failed, therefore {} \
-                            will be considered last in ordering compared to entries without issues\n\
-                            The error that happened was {}",
-                            &time_field_name,
-                            &lhs_entry.path().display(),
-                            &lhs_entry.path().display(),
-                            lhs_time_error
-                        );
-                        Ordering::Less
-                    }
-                    (Err(lhs_time_error), Err(rhs_time_error)) => {
-                        eprintln!(
-                            "Error: Fetching the {} for the files {} and {} failed, therefore they \
-                             will be considered equal but last overall in ordering compared to entries without issues\n\
-                             The errors that happened were \n{} and \n{} respectively.",
-                            &time_field_name,
-                            &lhs_entry.path().display(),
-                            &rhs_entry.path().display(),
-                            lhs_time_error,
-                            rhs_time_error
-                        );
-                        Ordering::Equal
-                    }
-                }
-            }
-            (Ok(_), Err(rhs_metadata_error)) => {
-                eprintln!(
-                    "Error: Fetching the metadata for the file {} failed, therefore {} will be considered last in ordering compared to entries without issues\n\
-                    The error that happened was {}",
-                    &rhs_entry.path().display(),
-                    &rhs_entry.path().display(),
-                    rhs_metadata_error
-                );
-                Ordering::Greater
-            }
-            (Err(lhs_metadata_error), Ok(_)) => {
-                eprintln!(
-                    "Error: Fetching the metadata for the file {} failed, therefore {} will be considered last in ordering compared to entries without issues\n\
-                    The error that happened was {}",
-                    &lhs_entry.path().display(),
-                    &lhs_entry.path().display(),
-                    lhs_metadata_error
-                );
-                Ordering::Less
-            }
-            (Err(lhs_metadata_error), Err(rhs_metadata_error)) => {
-                eprintln!(
-                    "Error: Fetching the metadata for the files {} and {} failed, therefore {} amd {} will be considered equal in ordering compared to entries without issues\n\
-                    The errors that happened were \n{} and \n{} respectively",
-                    &lhs_entry.path().display(),
-                    &rhs_entry.path().display(),
-                    &lhs_entry.path().display(),
-                    &rhs_entry.path().display(),
-                    lhs_metadata_error,
-                    rhs_metadata_error
-                );
-                Ordering::Equal
-            }
-        }
-    }
 }
--- HelperPrograms/mason/src/processing/root_element_processors.rs
+++ HelperPrograms/mason/src/processing/root_element_processors.rs
@@ -3,10 +3,14 @@ use crate::arguments::arguments::FileFolderItemLocation;
 use crate::processing::directory_processors::{
     process_directory_as_chunks, process_directory_in_order_of_sorting_algorithm,
 };
+use crate::processing::verbose_logging::verbose_log;
 use std::fs;
 use std::path::Path;
 
 pub(super) fn process_root_directory(arguments: &arguments::Arguments, path: &Path) -> usize {
+    verbose_log("BEGINNING PROCESSING OF ROOT DIRECTORY");
+    verbose_log("RETRIEVING ITERATOR OF ROOT DIRECTORY'S CHILDREN");
+
     let result = path.read_dir();
     if result.is_err() {
         eprintln!(
@@ -21,11 +25,14 @@ pub(super) fn process_root_directory(arguments: &arguments::Arguments, path: &Pa
     }
 
     let mut read_dir = result.unwrap();
-    
-    if !matches!(
+    verbose_log("RETRIEVED ITERATOR OF ROOT DIRECTORY'S CHILDREN");
+
+    verbose_log("EXAMINING DIRECTORY PROCESSOR TO USE BASED ON FILE/FOLDER GROUPING DESIRED");
+    let result = if !matches!(
         arguments.file_folder_item_location(),
         FileFolderItemLocation::InOrderOfSortingAlgorithm
     ) {
+        verbose_log("SELECTED IN ORDER OF SORTING ALGORITHM DIRECTORY PROCESSOR");
         process_directory_in_order_of_sorting_algorithm(
             arguments,
             &mut read_dir,
@@ -33,14 +40,30 @@ pub(super) fn process_root_directory(arguments: &arguments::Arguments, path: &Pa
             path,
         )
     } else {
+        verbose_log("SELECTED CHUNKED DIRECTORY PROCESSOR");
         process_directory_as_chunks(arguments, &mut read_dir, arguments.max_depth(), path)
-    }
+    };
+
+    verbose_log(&format!(
+        "PROCESSING FINISHED (FROM ROOT DIRECTORY PROCESSOR). NUMBER OF FILES PROCESSED IS {}",
+        result
+    ));
+    result
 }
 
 pub(super) fn process_file(arguments: &arguments::Arguments) -> usize {
+    verbose_log("FILE PROCESSOR STARTING");
+    verbose_log("RETRIEVING FILE PATH");
     let file = arguments.path().as_deref().unwrap();
     let path = Path::new(file);
     let path_buffer = path.to_path_buf();
+
+    verbose_log(&format!(
+        "RETRIEVED FILE PATH AS PATH BUFFER WITH VALUE '{}'",
+        file
+    ));
+    verbose_log("CALCULATING PARENT DIRECTORY");
+    verbose_log("CANONICALIZING PATH BUFFER");
     let potential_parent_buffer = fs::canonicalize(path_buffer.clone());
 
     if potential_parent_buffer.is_err() {
@@ -58,7 +81,15 @@ pub(super) fn process_file(arguments: &arguments::Arguments) -> usize {
         return 0;
     }
 
+    verbose_log("CALCULATED CANONICALIZED PATH BUFFER");
+
     let parent_buffer = potential_parent_buffer.unwrap();
+    verbose_log(&format!(
+        "CALCULATED PARENT DIRECTORY AS '{}'",
+        parent_buffer.display()
+    ));
+
+    verbose_log("OBTAINING DIRECTORY READER");
     let potential_directory_reader = parent_buffer.read_dir();
     if potential_directory_reader.is_err() {
         eprintln!(
@@ -70,15 +101,30 @@ pub(super) fn process_file(arguments: &arguments::Arguments) -> usize {
     }
 
     let mut directory_reader = potential_directory_reader.unwrap();
+    verbose_log("DIRECTORY READER OBTAINED");
+
+    verbose_log(
+        "EXAMINING DIRECTORY PROCESSOR TO USE BASED ON FILE/FOLDER GROUPING DESIRED FOR DIRECTORY OF meson.build AND NOT FOR SUB-DIRECTORIES",
+    );
 
     let result = if !matches!(
         arguments.file_folder_item_location(),
         FileFolderItemLocation::InOrderOfSortingAlgorithm
     ) {
+        verbose_log(
+            "SELECTED IN ORDER OF SORTING ALGORITHM DIRECTORY PROCESSOR FOR DIRECTORY OF meson.build AND NOT FOR SUB-DIRECTORIES",
+        );
         process_directory_in_order_of_sorting_algorithm(arguments, &mut directory_reader, 1, path)
     } else {
+        verbose_log(
+            "SELECTED CHUNKED DIRECTORY PROCESSOR FOR DIRECTORY OF meson.build AND NOT FOR SUB-DIRECTORIES",
+        );
         process_directory_as_chunks(arguments, &mut directory_reader, 1, path)
     };
 
+    verbose_log(&format!(
+        "DIRECTORY PROCESSING COMPLETE (FROM FILE PROCESSOR). PROCESSED {}",
+        result
+    ));
     result
 }
